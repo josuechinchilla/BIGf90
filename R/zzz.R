ca5a05e35853c5fdc912b72c10a2ebaf07bd2f18
Josue Chinchilla-Vargas <jc3635@cornell.edu>
2024-08-08 14:54:46 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
  "BIGf90 Reference: \n",
   "Chinchilla-Vargas J, Taniguti C, Sandercock A, Breeding Insight Team (2024). _BIGf90:
   Breeding Insight Genomics R front face to blupf90 modules. R package version 0.3.2,
-  <https://github.com/Breeding-Insight/BIGr> \n",
+  <https://github.com/Breeding-Insight/BIGr> \n\n",
   "BLUPF90 Reference: \n",
   "Misztal, I., S. Tsuruta, D.A.L. Lourenco, I. Aguilar, A. Legarra, and Z. Vitezica. 2014.\n",
   "Manual for BLUPF90 family of programs: http://nce.ads.uga.edu/wiki/lib/exe/fetch.php?media=blupf90_all2.pdf")
